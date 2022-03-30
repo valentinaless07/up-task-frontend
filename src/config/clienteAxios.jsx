f5296b169c7da8e924c1b4d3fc71658ff88109ec
valentin <valentinaless07@hotmail.com>
2022-03-30 00:35:27 -0300

    deploy

--- src/config/clienteAxios.jsx
+++ src/config/clienteAxios.jsx
@@ -2,10 +2,7 @@ import axios from "axios";
 
 const clienteAxios = axios.create({
     baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
-    headers: {
-        'Access-Control-Allow-Origin' : '*',
-        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-        }
+   
 })
 
 export default clienteAxios
